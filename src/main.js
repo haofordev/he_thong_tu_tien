73d36d82df730d71a31f618d055da12cbe8efb7f
hao <hao@innorix.com>
2026-04-10 14:31:27 +0700

    a

--- src/main.js
+++ src/main.js
@@ -33,10 +33,20 @@ const mapSequence = [
     "sect_lk_c03",
     "sect_lk_c04",
     "sect_lk_c05",
+    "sect_lk_c06",
+    "sect_lk_c07",
+    "sect_lk_c08",
+    "sect_lk_c09",
+    "sect_lk_c10",
     "train_lk_02",
     "train_lk_03",
     "train_lk_04",
     "train_lk_05",
+    "train_lk_06",
+    "train_lk_07",
+    "train_lk_08",
+    "train_lk_09",
+    "train_lk_10",
 ];
 
 //const mapSequence = ["starter_01"];
